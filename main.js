40569d9d9fe1bd3d04c3f711060e5e786d7b797e
tonitruum <104451675+tonitruum-0@users.noreply.github.com>
2023-01-16 01:03:52 -0500

    asdsfghj

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-document.getElementById("container").innerHTML = "Hello World";
+import * as arr from './array.js'
+
+let selectors = {
+    container: document.getElementById('container'),
+}
+let i = 0;
+arr.landscapes.forEach(l => {
+    i++;
+    selectors.container.innerHTML += `<img class="lscape" id='mtn${i}' src="${l}">`;
+    console.log(selectors.container);
+});
